d8ac1ff62b6f17075e01808a427a304d94027997
jokinghero <kornellabun@gmail.com>
2015-02-01 00:25:05 +0100

    solved issues with production env

--- config/env/production.js
+++ config/env/production.js
@@ -22,14 +22,25 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.min.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+				'public/lib/fontawesome/css/font-awesome.min.css',
+				'public/lib/slider-pro/dist/css/slider-pro.min.css',
+				'public/lib/ngDialog/css/ngDialog.min.css',
+				'public/lib/ngDialog/css/ngDialog-theme-plain.min.css'
 			],
 			js: [
+				'public/lib/jquery/dist/jquery.min.js',
+				'public/lib/jquery-ui/jquery-ui.min.js',
 				'public/lib/angular/angular.min.js',
 				'public/lib/angular-resource/angular-resource.min.js',
 				'public/lib/angular-animate/angular-animate.min.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+				'public/lib/angular-dragdrop/src/angular-dragdrop.min.js',
+				'public/lib/slider-pro/dist/js/jquery.sliderPro.min.js',
+				'public/lib/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js',
+				'public/lib/ngDialog/js/ngDialog.min.js',
+				'public/lib/bootstrap/js/tooltip.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
--- config/env/secure.js
+++ config/env/secure.js
@@ -26,6 +26,7 @@ module.exports = {
 			],
 			js: [
 				'public/lib/angular/angular.min.js',
+				'public/lib/angular-route/angular-route.min.js',
 				'public/lib/angular-resource/angular-resource.min.js',
 				'public/lib/angular-animate/angular-animate.min.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',

223d67d3b3bf3e05c00d179894cf6f7b1ead0578
jokinghero <kornellabun@gmail.com>
2015-02-08 19:08:10 +0100

    mandrill to is an array

--- app/services/mandrill.js
+++ app/services/mandrill.js
@@ -4,16 +4,14 @@ var config = require('../../config/config');
 var mandrill = require('mandrill-api/mandrill');
 var mandrill_client = new mandrill.Mandrill(config.mandrill);
 exports.send = function(fromName, fromEmail, to, subject, text, tags) {
-    console.log(config);
     var message = {
         'html': null,
         'text': 'test',
         'subject': subject,
         'from_email': fromEmail,
         'from_name': fromName,
-        'to': to
+        'to': [to]
     };
-    console.log(message);
     mandrill_client.messages.send({
         'message': message,
         'async': false,
@@ -22,7 +20,6 @@ exports.send = function(fromName, fromEmail, to, subject, text, tags) {
     }, function(result) {
         console.log('[Mandrill API] Mandrill API called. %j', result);
     }, function(e) {
-        console.log(e);
         console.error('[Error] A mandrill error occurred: ' + e.name + ' - ' + e.message);
     });
 };

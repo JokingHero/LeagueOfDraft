18ec6600c7ca71b8bdcf2d0d47a113bd1305941f
jokinghero <kornellabun@gmail.com>
2015-05-22 12:04:51 +0200

    cant send an email bagilion times

--- public/modules/core/controllers/footer.client.controller.js
+++ public/modules/core/controllers/footer.client.controller.js
@@ -1,10 +1,16 @@
 'use strict';
 
-angular.module('core').controller('FooterController', ['$scope', '$http', 'ngDialog',
-    function($scope, $http, ngDialog) {
+angular.module('core').controller('FooterController', ['$scope', '$http', 'ngDialog', 'toaster',
+    function($scope, $http, ngDialog, toaster) {
 
-    	$scope.email = function() {
+        $scope.email = function() {
             $http.post('/contact', $scope.data).success(function(response) {
+                $scope.closeThisDialog();
+                toaster.pop({
+                    type: 'success',
+                    title: 'Message',
+                    body: response.message
+                });
                 $scope.success = response.message;
             }).error(function(response) {
                 $scope.error = response.message;
